926d9b84335f6d174d66a1640e06dab9d7e0da44
haikaljs <haikaljavascript@gmail.com>
2024-02-06 20:48:56 +0800

    Create TopHeader component

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -1,23 +1,27 @@
 <template>
- <div class="min-h-full flex">
-    <!-- sidebar -->
-   <Sidebar/>
-    <!-- end sidebar -->
-    <div class="flex-1">
-        <header class="h-8 shadow bg-white">Header</header>
-        <!-- content -->
-        <main>
-            <router-view></router-view>
-        </main>
-        <!-- end content -->
+    <div class="min-h-full flex">
+        <!-- sidebar -->
+        <Sidebar />
+        <!-- end sidebar -->
+        <div class="flex-1">
+            <TopHeader />
+            <!-- content -->
+            <main class="p-6">
+
+                <router-view></router-view>
+
+
+            </main>
+            <!-- end content -->
+        </div>
+
     </div>
-    
- </div>
 </template>
 
 <script setup>
 import Sidebar from "../components/Sidebar.vue"
-const {title} = defineProps({
+import TopHeader from "./TopHeader.vue";
+const { title } = defineProps({
     title: String
 })
 </script>
